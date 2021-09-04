88c4916a1111e42fb696c59c07884d83f3df139c
hu-9836 <88919367+hu-9836@users.noreply.github.com>
2021-09-04 12:05:53 +0800

    Add files via upload

---
+++ 冒泡排序.cpp
@@ -0,0 +1,33 @@
+#include<stdio.h> 
+void bubble_sort(int arr[],int sz)
+{
+	int x=0;
+	int y=0;
+	int z=0;
+	//que ding pai xu de tang shu 
+	for(x=0;x<sz;x++)
+	{
+		//mei yi tang pai xu
+		for(y=0;y<sz-1-x;y++)
+		{
+			if(arr[y]>arr[y+1])
+			{
+				z=arr[y];
+				arr[y]=arr[y+1];
+				arr[y+1]=z;
+			}
+		}
+	}
+}
+int main()
+{
+	int  arr[]={9,8,7,6,5,4,3,2,1,0};
+	int  sz=sizeof(arr)/sizeof(arr[0]);
+	int  a=0;
+	bubble_sort(arr,sz);
+	for(a=0;a<sz;a++)
+	{
+		printf("%d ",arr[a]);
+	}
+	return 0;
+}
