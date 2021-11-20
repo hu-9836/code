d314d2548684b5236c431f4dca7d8100345d2858
hu-9836 <88919367+hu-9836@users.noreply.github.com>
2021-11-20 20:42:45 +0800

    Add files via upload

--- 求一个数中1的个数.cpp
+++ 求一个数中1的个数.cpp
@@ -1,16 +1,34 @@
 #include <stdio.h>
-int main()
+int fac(unsigned int x)
 {
-	int x,y=0;
-	scanf("%d",&x);
-	while(x!=0)
+	int y=0;
+	while(x)
 	{
 		if(x%2==1)
 		{
 			y++;
 		}
 		x=x/2;
-	} 
+	}
+	return y;
+ } 
+ /*
+int fac(unsigned int x)
+{
+	int y=0,a;
+	for(a=0;a<32;a++)
+	{
+		if(((x>>a)&1)==1)
+			y++;
+	}
+	return y;
+ }
+ */
+int main()
+{
+	int x,y=0;
+	scanf("%d",&x);
+	y=fac(x);
 	printf("y=%d\n",y);
 	return 0;
 }
